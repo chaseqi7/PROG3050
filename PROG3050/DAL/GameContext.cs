cd10e95d2158fc6fcca79acdc5d3f9923c2adac1
agalka1632 <agalka1632@conestogac.on.ca>
2017-10-06 11:50:15 -0400

    Entity Framework shit
    can view games list now

---
+++ PROG3050/DAL/GameContext.cs
@@ -0,0 +1,25 @@
+﻿using PROG3050.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace PROG3050.DAL
+{
+    public class GameContext : DbContext
+    {
+    
+        public GameContext() : base("CVGSContext")
+        {
+        }
+
+    public DbSet<Game> Games { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+    }
+}
+}
---
+++ PROG3050/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PROG3050.Models
+{
+    public class Game
+    {
+        [Key]
+        public Guid Guid { get; set; } 
+        public string EnglishName { get; set; }
+        public string EnglishDescription { get; set; }
+        public string EnglishDetail { get; set; }
+        
+    }
+}
