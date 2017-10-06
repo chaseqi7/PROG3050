40fbdacfb73f7fee246cfa3f896aef9e173063fc
Chase Qi <qiziming1@gmail.com>
2017-10-06 13:30:12 -0400

    Added Create Event Table.sql

---
+++ Scripts/Create Event Table.sql
@@ -0,0 +1,32 @@
+
+PRINT ''
+PRINT 'Data Tables.sql Starting';
+
+USE CVGS;
+
+GO
+
+--********************
+-- Tables about places
+--********************
+
+--=========
+-- Location
+--=========
+
+PRINT '>>> Creating Event Table';
+
+CREATE TABLE Event
+(
+ EventiID int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+ Name VarChar(200),
+ Description Varchar(2000),
+ StartDate Date,
+ EndDate Date,
+ Country Varchar(50),
+ StateProvice Varchar(100),
+ City Varchar(100),
+ Address Varchar(100)
+ );
+
+GO
