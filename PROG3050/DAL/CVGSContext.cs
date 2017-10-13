f0082cc0b9d00be2824ff6641ff8cbf2745d04e9
agalka1632 <agalka1632@conestogac.on.ca>
2017-10-13 13:39:59 -0400

    Changed Games to work with new data model

--- PROG3050/DAL/CVGSContext.cs
+++ PROG3050/DAL/CVGSContext.cs
@@ -17,7 +17,6 @@ namespace PROG3050.DAL
 
         public DbSet<Game> Games { get; set; }
         public DbSet<Event> Events { get; set; }
-        public DbSet<GameCategory> GameCategory { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
     {
         modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
